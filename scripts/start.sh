b1b2ecf081eb0f2172cdf52b47db942916275468
alonso.reynoso <alonso.reynoso@bbva.com>
2020-11-01 01:30:59 -0500

    Test run 13

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar aws-pipeline-example-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar /home/ec2-user/aws-pipeline-example-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
