c65cc11efa23f1af31203184946ea89b87fe3df4
alonso.reynoso <alonso.reynoso@bbva.com>
2020-10-31 23:09:01 -0500

    Test run

--- scripts/add_privileges.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-chmod +x /home/ec2-user/aws-pipeline-example-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start.sh
-chmod +x /home/ec2-user/stop.sh
--- scripts/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar /home/ec2-user/aws-pipeline-example-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
