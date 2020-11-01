42c0a14872dd1ce32b10bf8681c86a2acae4f168
alonso.reynoso <alonso.reynoso@bbva.com>
2020-10-31 21:12:22 -0500

    Initial commit

---
+++ src/main/java/pe/com/alonso360rn/AwsPipelineExampleApplication.java
@@ -0,0 +1,13 @@
+package pe.com.alonso360rn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsPipelineExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsPipelineExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/alonso360rn/AwsPipelineExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.alonso360rn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsPipelineExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
